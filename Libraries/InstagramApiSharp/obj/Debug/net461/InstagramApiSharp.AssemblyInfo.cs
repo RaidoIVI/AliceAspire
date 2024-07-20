6411fb602de5a9a2450b566203df60c1f79b3145
Ivan <kiba82@gmail.com>
2024-07-20 21:14:55 +0300

    rider

--- Libraries/InstagramApiSharp/obj/Debug/net461/InstagramApiSharp.AssemblyInfo.cs
+++ Libraries/InstagramApiSharp/obj/Debug/net461/InstagramApiSharp.AssemblyInfo.cs
@@ -33,7 +33,7 @@ Support:
 
 See example projects and wiki pages to find out how this library works.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.7.4.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.7.4.0+396f673040720bfa9bb2ba2431f7012942131915")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.7.4.0+a46b343792771aef90e79c8aa38feb2bb675681d")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InstagramApiSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.7.4.0")]
 [assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/ramtinak/InstagramApiSharp/")]
